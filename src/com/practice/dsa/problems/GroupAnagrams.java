2e960c6e7bcc871952368160cc22d79228c6003a
Sagnik Das <sagnik.das@hcl.com>
2022-06-08 00:26:00 +0530

    Group anagram comments updated

--- src/com/practice/dsa/problems/GroupAnagrams.java
+++ src/com/practice/dsa/problems/GroupAnagrams.java
@@ -16,18 +16,22 @@ public class GroupAnagrams {
 
         Map<String, List<String>> map = new HashMap<>();
 
+        //Iterate over the words
         for(String word: words){
+            //take each word and sort it by its characters - "cat" -> "act"
             char[] array = word.toCharArray();
             Arrays.sort(array);
 
             String key = new String(array);
 
+            //populate a map, if the key is not present in it
             if(!map.containsKey(key))
                 map.put(key, new LinkedList<String>());
 
+            //helps in grouping the anagrams - (key -> list of String values)
             map.get(key).add(word);
         }
 
-        return new LinkedList<>(map.values());
+        return new LinkedList<>(map.values()); // returns a List of List<String>
     }
 }
